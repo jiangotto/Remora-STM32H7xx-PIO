f02531e613ac5ab6ba79a7c136a24fd3036d4f26
Scott <scottalford75@gmail.com>
2025-07-12 12:40:20 +1000

    Refactored SPI

--- Src/remora-hal/STM32H7_SPIComms.h
+++ Src/remora-hal/STM32H7_SPIComms.h
@@ -30,19 +30,8 @@ private:
     volatile rxData_t*  		ptrRxData;
     volatile txData_t*  		ptrTxData;
     volatile DMA_RxBuffer_t* 	ptrRxDMABuffer;
+
     SPI_TypeDef*        		spiType;
-
-    uint8_t						RxDMAmemoryIdx;
-    uint8_t						RXbufferIdx;
-    bool						copyRXbuffer;
-
-	ModuleInterrupt<STM32H7_SPIComms>*	NssInterrupt;
-    ModuleInterrupt<STM32H7_SPIComms>*	dmaTxInterrupt;
-	ModuleInterrupt<STM32H7_SPIComms>*	dmaRxInterrupt;
-	IRQn_Type					irqNss;
-	IRQn_Type					irqDMArx;
-	IRQn_Type					irqDMAtx;
-
     SPI_HandleTypeDef   		spiHandle;
     DMA_HandleTypeDef   		hdma_spi_tx;
     DMA_HandleTypeDef   		hdma_spi_rx;
@@ -64,27 +53,39 @@ private:
     Pin*                        clkPin;
     Pin*                        csPin;
 
-    uint8_t						interruptType;
+    uint8_t						RxDMAmemoryIdx;
+    uint8_t						RXbufferIdx;
+    bool						copyRXbuffer;
 
+	ModuleInterrupt<STM32H7_SPIComms>*	NssInterrupt;
+    ModuleInterrupt<STM32H7_SPIComms>*	dmaTxInterrupt;
+	ModuleInterrupt<STM32H7_SPIComms>*	dmaRxInterrupt;
+
+	IRQn_Type					irqNss;
+	IRQn_Type					irqDMArx;
+	IRQn_Type					irqDMAtx;
+
+    uint8_t						interruptType;
     bool						newWriteData;
 
-    SPIName spi_get_peripheral_name(PinName mosi, PinName miso, PinName sclk);
+    SPIName getSPIPeripheralName(PinName mosi, PinName miso, PinName sclk);
+    Pin* createPin(const std::string& portAndPin, PinName pinName, const PinMap* map);
+    void enableSPIClock(SPI_TypeDef* instance);
+    void initDMA(uint32_t txRequest, uint32_t rxRequest);
 
 	HAL_StatusTypeDef startMultiBufferDMASPI(uint8_t*, uint8_t*, uint8_t*, uint8_t*, uint16_t);
 	int getActiveDMAmemory(DMA_HandleTypeDef*);
 
-	int DMA_IRQHandler(DMA_HandleTypeDef *);
+	int handleDMAInterrupt(DMA_HandleTypeDef *);
 	void handleRxInterrupt(void);
 	void handleTxInterrupt(void);
 	void handleNssInterrupt(void);
 
 public:
-	STM32H7_SPIComms(volatile rxData_t*, volatile txData_t*, SPI_TypeDef*);
     STM32H7_SPIComms(volatile rxData_t*, volatile txData_t*, std::string, std::string, std::string, std::string);
 	virtual ~STM32H7_SPIComms();
 
     void init(void);
-    void init_old(void);
     void start(void);
     void tasks(void);
 };
